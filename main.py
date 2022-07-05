0ed96009033a30202a55ec61c21f7782e89029d4
victor <em3888@gmail.com>
2022-07-05 10:17:24 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+
+def main():
+    st.markdown("## AirMusic - ")
+    st.markdown("""## AirMusic - The Perfect Companion for Music Practice
+
+Statement:
+Learning music is a long journey, good practice habits are essential.
+
+Thousands of students learn music everyday, and they all can benefit from developing enhanced practice techniques.
+
+AirMusic uses AI and Algo to study music and help students identify if they -
+
+play too high or too low in pitch;
+play too fast or too slow for the rhythms;
+or make errors which don’t make good musical sense.
+
+The app. targets many standard set exam. pieces, for example Grades 3, 5 and 8 for violin and other instruments.
+
+Simply click start and play, and your results are returned instantly. 
+
+Features:
+- Family report for parents;
+- Opera Track reports for multiple instruments;
+- Special Read-out notes function for eyesight impaired users.
+""")
+
+
+if __name__ == '__main__':
+    main()
