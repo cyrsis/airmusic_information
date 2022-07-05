52f8dcf10782423b85bc8e1025b27f81628683a1
victor <em3888@gmail.com>
2022-07-06 06:37:05 +0800

    Add Dev Environment

---
+++ pages/DevReference.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.write(f"### Dev Reference")
+st.write(f"#### Streamlit")
+
+streamlitLink = '[Steamlit API Reference](https://docs.streamlit.io/library/api-reference)'
+st.markdown(streamlitLink, unsafe_allow_html=True)
+st.markdown('---')
+
+st.write(f"### Style Guide")
+st.write(f"#### Markdown as much as you ")
---
+++ pages/OfficeLocation.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.write(f"### office Location")
+st.write(f"#### Kowloon Bay")
+st.write(f"Unit 1723, Metro Centre II, 21 Lam Hing Street, Kowloon Bay, HK")
+# Set the device
+
+st.markdown('---')
