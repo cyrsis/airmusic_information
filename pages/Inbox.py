c72c296e97003f87e0ed1165a49dca21326bf83c
timwong1155110844 <78635147+timwong1155110844@users.noreply.github.com>
2022-07-06 14:52:42 +0800

    Update Inbox.py

--- pages/Inbox.py
+++ pages/Inbox.py
@@ -4,12 +4,14 @@ st.markdown('Some Ideas')
 st.markdown("""
 
 Update Main:
+
 -> Play too high or low in pitch.
 - Plan A: Comparing the user's recording with the sample piece and selecting the difference among them. Data are digitalized.
-- Plan B: Using music21 to read the piece and generate the demo of the performance. Then, comparing the user's performance with music21's sample. -> this can be a solution for tuner.
+- Plan B: Using music21 to read the piece and generate the demo of the performance. Then, comparing the user's performance with music21's sample. (this can be a solution for tuner.)
 -> Play too fast or slow for the rhythms.
 
 Additional ideas to Main.
+
 (3) Some basic information of sound/music:
 - Reference:　Barry Parker, <Good Vibrations: The Physics of Music>, Johns Hopkins University Press, 2009-11-17, pp. 63.
 - The same note sounds differently in shape with different instruments, for instant, violin and piano.
@@ -18,6 +20,7 @@ Additional ideas to Main.
 
 
 Ideas with Streamlit:
+
 (1) Adding animation:
 - using Lottie in Streamlit and design a animation for AirMusic Icon/Logo. It can be an eye-catching image when user visit the webpage.
 
