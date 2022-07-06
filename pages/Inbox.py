d027f3b0775556dc113d2b16e52fa38a549c4ca0
timwong1155110844 <78635147+timwong1155110844@users.noreply.github.com>
2022-07-06 14:13:04 +0800

    Update Inbox.py

--- pages/Inbox.py
+++ pages/Inbox.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.markdown('Some Ideas')
+st.markdown("""
+
+Additional ideas to Main.
+(3) Some basic information of sound/music:
+- Reference:　Barry Parker, <Good Vibrations: The Physics of Music>, Johns Hopkins University Press, 2009-11-17, pp. 63.
+
+Ideas with Streamlit:
+(1) Adding animation:
+- using Lottie in Streamlit and design a animation for AirMusic Icon/Logo. It can be an eye-catching image when user visit the webpage.
