c6d16584d476e7dac53c1fcc111422d21fd8aa2d
timwong1155110844 <78635147+timwong1155110844@users.noreply.github.com>
2022-07-07 11:56:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ st.set_page_config(
     page_title="AirMusic ML-Core", page_icon="📊", initial_sidebar_state="expanded"
 )
 
+latest_iteration = st.empty()
+bar = st.progress(0)
+for i in range(100):
+    latest_iteration.text(f'目前進度 {i+1} %')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
 st.markdown("### AirMusic ")
 st.markdown("###### The Perfect Companion for Music Practice ")
 st.markdown('---')
