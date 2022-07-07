9866e14c7b41c7a915a2711e779330a360b7490f
timwong1155110844 <78635147+timwong1155110844@users.noreply.github.com>
2022-07-07 14:03:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -94,6 +94,47 @@ Features:
 - Starting at a quiet environment. Assume these do not exist external noise.
 - All calculations and testings may not be confirmed before the demo. 
 
+""")
+
+st.markdown('---')
+
+st.markdown("""
+                        
+##### Summarizing PitchDeck
+
+The user plays their musical instrument to Air Music App according to the **displayed music scores** and the App will produce **an instant report analysing pitch and rhythm**, **pinpointing on the music exact errors and areas for improvement**. It includes 1. text prompt and 2. voice-over features (in different languages in the future): this important feature will particularly assist users with special needs.
+
+With results accumulated during each term, parents and prospective teachers will have a more precise guide for school entry, and students can progress more confidently to higher grades with confirmed, reliable technical assurance. Air Music App can **inspire effective practice**, **provide an energising spur to practise**, and **lead to enhanced learning and growth in self-confidence**; families will know more certainly when the student is ready for the next grade exam. and which level to tackle next - leading to **a welcome reduction in over-ambitious teacher & parent expectations**. This will **train better practice habits**, **develop self-awareness of how to learn, what to listen for and how to listen, even, maybe, how to teach better**; and parents can check the teacher is doing the right things.
+
+Air Music App will help learners:
+
+- track progress
+
+- highlight areas for improvement
+
+- offer gamification features [1]  
+
+it can encourage learners to practise more, at a readily affordable amount for any average music learner. Hong Kong alone has more than 1M music learners; it will therefore only take 2% of these learners to subscribe to the App to deliver a decent ROI.
+
+part from regular fee income, we will have music examination pieces and masterclasses performed by renowned educators. Examination repertoire can be included for more advanced learners, who will pay subscriptions on a per-piece or per-grade basis, while masterclasses will collect an annual subscription.
+
+Air Music App can also be tailor-made [2] to schools’ requirements, such as student orchestras and choirs, where conductors can monitor the performing quality of each player and singer using our App; since every school has an orchestra or choir - often both - and most of them compete regularly in the annual School Speech and Music Festivals. Our App will help extensively, particularly during and after COVID with the consequent knock-on online educational developments.
+
+Additional future features will cover Chinese instruments [3] and learners. There is a huge free classical music database which we can match with the examination board requirements in the US, Canada, and China.
+
+The community of music learners [4] offers significant cross-selling opportunities with concert and event organisers, music instrument and music score merchants and distributors, and music education and entertainment institutions.
+
+Air Music App is **“The Smartest AI for Effective Music Practice”**.
+
+Remarks:
+
+[1] Gamification features will be discuss after the app is done and the efficiency of the app is high enough. Otherwise, gamification features may cause time-consumed situation.
+
+[2] Idea:　serivce for a specific instrument (violin, piano, flute...) or orchestras and choirs as a whole.
+
+[3] It is necessary to study the differences between Western instrument and Chinese instrument, depend on the sound waves and amplitudes produced are independent or not. Then, new database might need to build.
+
+[4] It is hard to create until the app is familiar in the market and amount of users are using it. Community cannot be achieve in the current stage.
 
 """)
 
