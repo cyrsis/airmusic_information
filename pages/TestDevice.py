807ae6843a6f6cbf2e20dddc3b6891121505cbc5
victor <em3888@gmail.com>
2022-07-08 16:11:14 +0800

    Fix the email for Sam

--- pages/TestDevice.py
+++ pages/TestDevice.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import streamlit as st
 
-st.write('Sam - iphone 8 plus - apple id: i609966495@gmail.com')
+st.write('Sam - iphone 8 plus - apple id: i60996395@gmail.com')
 st.write('The lady - dont care - apple id: chloeccy7@gmail.com')
 st.write('Tim - S20- Android 12 - wonghoiman18@gmail.com')
 
