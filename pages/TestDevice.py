aeb34e18d7be7e91ffa4cd035a4e3a9938b1a300
victor <em3888@gmail.com>
2022-07-08 15:50:35 +0800

    Add the Web Version of the App for the UI testing

--- pages/TestDevice.py
+++ pages/TestDevice.py
@@ -5,3 +5,6 @@ import streamlit as st
 st.write('Sam - iphone 8 plus - apple id: i609966495@gmail.com')
 st.write('The lady - dont care - apple id: chloeccy7@gmail.com')
 st.write('Tim - S20- Android 12 - wonghoiman18@gmail.com')
+
+st.write('Link for AppTeser')
+st.write('https://appdistribution.firebase.dev/i/804c92ec1d6797ff')
