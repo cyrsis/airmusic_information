de88bb7945970323ceb54ed5a4ea017be20ed3c0
victor <em3888@gmail.com>
2022-07-08 14:10:27 +0800

    Add Test Device list and start working on the App

---
+++ pages/TestDevice.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+import streamlit as st
+
+st.write('Sam - iphone 8 plus - apple id: i609966495@gmail.com')
+st.write('The lady - dont care - apple id: chloeccy7@gmail.com')
+st.write('Tim - S20- Android 12 - wonghoiman18@gmail.com')
