c15dcfec29161a685065bcca3529cb5a2a8887d6
SamIp-ac <86075853+SamIp-ac@users.noreply.github.com>
2022-07-11 09:45:53 +0800

    Update Inbox.py

--- pages/Inbox.py
+++ pages/Inbox.py
@@ -108,7 +108,7 @@ st.markdown("""
    
 4)	If the player does not play with regular rhythm then the tempo is a minor problem! This situation creates real difficulties, I think, for the app accurately to track the errors of pitch, especially if the player misplays note-lengths and causes distortion to the number of notes per bar and number of bars altogether.
    
-   -
+   - 
    
 5)	To assess rhythmic errors (ignoring pitch errors for now) the app has to recognise a “right” note played for too short or too long a duration. Also, if the player is simply badly out of time and cannot maintain steady pulse, the app needs to be abl to adjust in some way so it can make a valid response on the rhythmic accuracy.
 
